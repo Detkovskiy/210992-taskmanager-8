1b450902ab66b496b7caa878e15890803ddcd012
Yura Detkovskiy <9119113142@mail.ru>
2019-03-01 12:52:00 +0300

    рабочая версия

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
-import getRandomNumber from '../src/utils.js';
+import {getRandomFromInterval} from '../src/utils.js';
 
 export const card = () => ({
   title: [
     `Изучить теорию`,
     `Сделать домашку`,
     `Пройти интенсив на соточку`,
-  ][getRandomNumber(0, 3)],
+  ][getRandomFromInterval(0, 3)],
   dueDate: Date.now() + 1 + Math.floor(((Math.random() > 0.5) ? 1 : -1) * (Math.floor(Math.random() * 7))) * 24 * 60 * 60 * 1000,
   tags: new Set([
     `homework`,
@@ -16,7 +16,7 @@ export const card = () => ({
   ]),
   picture: `//picsum.photos/100/100?r=${Math.random()}`,
   repeatingDays: {
-    'mo': getRandomNumber(0, 2)
+    'mo': getRandomFromInterval(0, 2)
   },
   color: [
     `black`,
@@ -24,7 +24,7 @@ export const card = () => ({
     `blue`,
     `green`,
     `pink`
-  ][getRandomNumber(0, 5)],
+  ][getRandomFromInterval(0, 5)],
   isFavorite: false,
   isDone: true
 });

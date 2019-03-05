8397e4592499c04fb40cc00c5622c862f6aac702
Yura Detkovskiy <9119113142@mail.ru>
2019-03-05 22:19:21 +0300

    дублирование методов обработки данных

--- src/card.js
+++ src/card.js
@@ -10,6 +10,7 @@ export class Card {
     this._color = data.color;
 
     this._element = null;
+    this._onEdit = null;
   }
 
   _isRepeated() {
@@ -65,10 +66,6 @@ export class Card {
                 <div class="card__settings">
                   <div class="card__details">
                     <div class="card__dates">
-                      <button class="card__date-deadline-toggle" type="button">
-                        date: <span class="card__date-status">yes</span>
-                      </button>
-
                       <fieldset class="card__date-deadline">
                         <label class="card__input-deadline-wrap">
                           <input class="card__date" type="text" placeholder="4 MARCH" name="date" value="${this._getDateDeadline()}">
@@ -76,29 +73,6 @@ export class Card {
                         <label class="card__input-deadline-wrap">
                           <input class="card__time" type="text" placeholder="11:15 PM" name="time" value="${this._getTimeDeadline()}">
                         </label>
-                      </fieldset>
-
-                      <button class="card__repeat-toggle" type="button">
-                        repeat:<span class="card__repeat-status">yes</span>
-                      </button>
-
-                      <fieldset class="card__repeat-days">
-                        <div class="card__repeat-days-inner">
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-mo-4" name="repeat" value="mo">
-                          <label class="card__repeat-day" for="repeat-mo-4">mo</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-tu-4" name="repeat" value="tu" checked="">
-                          <label class="card__repeat-day" for="repeat-tu-4">tu</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-we-4" name="repeat" value="we">
-                          <label class="card__repeat-day" for="repeat-we-4">we</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-th-4" name="repeat" value="th">
-                          <label class="card__repeat-day" for="repeat-th-4">th</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-fr-4" name="repeat" value="fr" checked="">
-                          <label class="card__repeat-day" for="repeat-fr-4">fr</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" name="repeat" value="sa" id="repeat-sa-4">
-                          <label class="card__repeat-day" for="repeat-sa-4">sa</label>
-                          <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-su-4" name="repeat" value="su" checked="">
-                          <label class="card__repeat-day" for="repeat-su-4">su</label>
-                        </div>
                       </fieldset>
                     </div>
 
@@ -115,10 +89,6 @@ export class Card {
                           </button>
                         </span>`).join(``)}
                       </div>
-
-                      <label>
-                        <input type="text" class="card__hashtag-input" name="hashtag-input" placeholder="Type new hashtag here">
-                      </label>
                     </div>
                   </div>
 
@@ -126,42 +96,44 @@ export class Card {
                     <input type="file" class="card__img-input visually-hidden" name="img">
                     <img src="${this._picture}" alt="task picture" class="card__img">
                   </label>
-
-                  <div class="card__colors-inner">
-                    <h3 class="card__colors-title">Color</h3>
-                    <div class="card__colors-wrap">
-                      <input type="radio" id="color-black-4" class="card__color-input card__color-input--black visually-hidden" name="color" value="black">
-                      <label for="color-black-4" class="card__color card__color--black">black</label>
-                      <input type="radio" id="color-yellow-4" class="card__color-input card__color-input--yellow visually-hidden" name="color" value="yellow" >
-                      <label for="color-yellow-4" class="card__color card__color--yellow">yellow</label>
-                      <input type="radio" id="color-blue-4" class="card__color-input card__color-input--blue visually-hidden" name="color" value="blue">
-                      <label for="color-blue-4" class="card__color card__color--blue">blue</label>
-                      <input type="radio" id="color-green-4" class="card__color-input card__color-input--green visually-hidden" name="color" value="green">
-                      <label for="color-green-4" class="card__color card__color--green">green</label>
-                      <input type="radio" id="color-pink-4" class="card__color-input card__color-input--pink visually-hidden" name="color" value="pink" checked="">
-                      <label for="color-pink-4" class="card__color card__color--pink">pink</label>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__status-btns">
-                  <button class="card__save" type="submit">save</button>
-                  <button class="card__delete" type="button">delete</button>
-                </div>
+                 </div>
               </div>
             </form>
           </article>`;
 
   }
 
-  render(container) {
-    if (this._element) {
-      container.removeChild(this._element);
-      this._element = null;
+  _onEditButtonClick() {
+    if (typeof this._onEdit === `function`) {
+      this._onEdit();
     }
+  }
 
+  get element() {
+    return this._element;
+  }
+
+  set onEdit(fn) {
+    this._onEdit = fn;
+  }
+
+  bind() {
+    this._element.querySelector(`.card__btn--edit`).addEventListener(`click`, this._onEditButtonClick.bind(this));
+  }
+
+  unbind() {
+    this._element.removeEventListener(`click`, this._onEditButtonClick.bind(this));
+  }
+
+  render() {
     this._element = createElement(this.template);
-    container.appendChild(this._element);
+    this.bind();
+    return this._element;
+  }
+
+  unRender() {
+    this.unbind();
+    this._element = null;
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import {render} from '../src/utils';
 import {renderFilter} from '../src/make-filter.js';
 import {getCard} from '../src/data.js';
 import {Card} from "./card";
+import {CardEdit} from "./cardEdit";
 
 const filtersSection = document.querySelector(`.main__filter`);
-const boardTasks = document.querySelector(`.board__tasks`);
+const boarCardTasks = document.querySelector(`.board__tasks`);
 const FILTER_NAME = [
   {
     label: `all`,
@@ -43,5 +44,20 @@ const FILTER_NAME = [
 /* Вывод фильтров на станицу */
 render(filtersSection, renderFilter(FILTER_NAME));
 
-const firstCard = new Card(getCard());
-firstCard.render(boardTasks);
+const cardTask = new Card(getCard());
+const editCardTask = new CardEdit(getCard());
+
+boarCardTasks.appendChild(cardTask.render());
+
+cardTask.onEdit = () => {
+  editCardTask.render();
+  boarCardTasks.replaceChild(editCardTask.element, cardTask.element);
+  cardTask.unRender();
+};
+
+editCardTask.onSubmit = () => {
+  cardTask.render();
+  boarCardTasks.replaceChild(cardTask.element, editCardTask.element);
+  editCardTask.unRender();
+};
+
